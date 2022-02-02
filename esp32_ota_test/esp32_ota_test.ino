d10ceaeece91b5d1fc59f8a3c03e611ef9040c5b
Aftab Usmani <usmani12345@gmail.com>
2022-02-01 19:16:56 -0800

    Update test.

--- esp32_ota_test/esp32_ota_test.ino
+++ esp32_ota_test/esp32_ota_test.ino
@@ -9,11 +9,11 @@ const char * password = "subjectsoftumble#";
 
 
 String FirmwareVer = {
-  "2.2"
+  "2.3"
 };
 #define LED_BUILTIN 2
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/dukegold/esp32_ota_test/main/esp32_ota_test/bin_ver.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/dukegold/esp32_ota_test/main/esp32_ota_test/build/esp32.esp32.esp32/esp32_ota_test.ino.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
@@ -55,7 +55,7 @@ void loop() {
    {
        Serial.println("wifi connected");
    }
-   else
+   else if(WiFi.status() != WL_CONNECTED)
    {
     connect_wifi();
    }
